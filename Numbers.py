1633ed038d84fcfefbae748c19fedc02e67d314c
Alfredo Bustamante <alfredobustamante@.mail.weber.edu>
2022-02-09 18:08:38 -0700

    Infromation about python numbers

---
+++ Numbers.py
@@ -0,0 +1,15 @@
+import random
+MyRandomNumber = random.randrange(1,20)
+
+
+MyFistNumber = 42
+MySecondNumber = 30.5
+print (type(MyFistNumber))
+print (type(MySecondNumber))
+print (type(MyRandomNumber))
+
+total = MyFistNumber + MySecondNumber
+print (total)
+print (type(total))
+
+print ("My Random Number is",MyRandomNumber) # Cast an alternate datatype
